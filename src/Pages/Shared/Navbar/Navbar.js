71a3904fd05393b74254f871fb4abbdb7a64dfaf
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-03-01 23:27:05 +0600

    component add

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,38 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const menuItem = <>
+    <li className='text-xl '><Link to="/">Home</Link></li>
+    <li className='text-xl '><Link to="/">Home</Link></li>
+    <li className='text-xl '><Link to="/">Home</Link></li>
+    <li className='text-xl '><Link to="/">Home</Link></li>
+  </>
   return (
     <div>
-    <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle">
-        
+     <div className="navbar sticky top-0">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn bg-black lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-        
-      </div>
-    </div>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-        </div>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+      <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-gray-400 rounded-box w-52">
+        {menuItem}
       </ul>
     </div>
+    <Link to='/' className="text-3xl font-semibold text-white">Online Explore</Link>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+  <ul className="menu menu-horizontal p-0 pl-96 ml-64">
+      {menuItem}
+    </ul>
+  
+  </div>
+  <div className="navbar-end">
+  <label htmlFor='dashboard-sidebar' tabIndex={0} className="btn bg-black lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
   </div>
 </div>
     </div>
