64c30a62b0e46ab0f3b67c9c8d624e2d15bf96e7
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-03-06 23:27:47 +0600

    css animation add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import 'animate.css';
 const Login = () => {
   return (
     <div>
+      <div>
+      <div className="animate__animated animate__backInDown card w-96 bg-black shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+      </div>
+    <h1 class="">An animated element</h1>
       <h1>This is login page</h1>
     </div>
   );
