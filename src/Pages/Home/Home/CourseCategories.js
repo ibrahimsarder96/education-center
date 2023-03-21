59d67fd46d88b63dfeea9c343a68fe34f5ac3a0e
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-03-21 23:55:34 +0600

    course categories add

---
+++ src/Pages/Home/Home/CourseCategories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseCategories = () => {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-3xl'>Course Categories</h1>
+    </div>
+  );
+};
+
+export default CourseCategories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import CourseCategories from './CourseCategories';
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <CourseCategories></CourseCategories>
     </div>
   );
 };
