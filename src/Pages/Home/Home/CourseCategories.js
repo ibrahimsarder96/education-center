24c46d631d27856102f9bdca17a9822186538121
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-03-22 23:57:18 +0600

    course details

--- src/Pages/Home/Home/CourseCategories.js
+++ src/Pages/Home/Home/CourseCategories.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import CourseDetails from './CourseDetails';
 
 const CourseCategories = () => {
+  let courses = [
+    {
+      id: 1,
+      Name: 'Web Development',
+      Language: 'Html, Css, JavaScript'
+    },
+    {
+      id: 2,
+      Name: 'Web Development',
+      Language: 'Html, Css, JavaScript'
+    },
+    {
+      id: 3,
+      Name: 'Web Development',
+      Language: 'Html, Css, JavaScript'
+    },
+  ]
   return (
     <div>
+      <h1 className='text-center font-bold text-xl text-orange-400'>..Popular Categories..</h1>
       <h1 className='text-center font-bold text-3xl'>Course Categories</h1>
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {
+          courses.map((course) => <CourseDetails
+          key={course.id}
+          course={course}
+          ></CourseDetails>)
+        }
+      </div>
     </div>
   );
 };
