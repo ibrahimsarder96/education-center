00acf08eb09685a37ec76b6109d4d5387603d7e0
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-08-27 23:56:54 +0600

    cardInfo change

--- src/Pages/Home/Home/CardInfo.js
+++ src/Pages/Home/Home/CardInfo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'animate.css';
-const InfoCard = ({bgclass, cardTitle, info}) => {
+const InfoCard = ({ cardTitle, info}) => {
   return (
     <div className={`animate__animated animate__bounceInUp card lg:card-side bg-base-100 shadow-xl ${bgclass}`}>
     <figure  className='pl-6 pt-4'>
