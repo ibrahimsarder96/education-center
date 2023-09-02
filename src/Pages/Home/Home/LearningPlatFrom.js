0e70762ef2578f09c20ed2b9fbb18c6911a89610
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-09-02 23:46:38 +0600

    learning video

--- src/Pages/Home/Home/LearningPlatFrom.js
+++ src/Pages/Home/Home/LearningPlatFrom.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import img1 from '../../../assets/achieve/m.jpg';
 import img2 from '../../../assets/achieve/w.jpg';
-import mission from '../../../assets/achieve/mission.svg';
-import vision from '../../../assets/achieve/vision.svg';
 import { ImCheckboxChecked } from 'react-icons/im';
+import './LearningPlatFrom.css';
 const LearningPlatFrom = () => {
   return (
     <div>
       <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-0 gap-4 mb-16 mt-16'>
-      
       <div>
         <h5 className='text-green-400 font-bold text-xl'>Best Online Learning Platform</h5>
         <h2 className='text-5xl font-bold my-5'>One Platfrom & Many</h2>
         <h2 className='text-5xl font-bold my-5'><span className='text-purple-600'>Courses</span> For You</h2>
 
         <div>
-          <div className='bg-orange-400 w-24 '></div>
+          <div className='bg-orange-400 w-24'></div>
         </div>
         <p className='text-xl text-gray-500 mt-4'>There Are Many Variations Of Passages Of Lorem Ipsum Available, But The Majority Have Suffered.</p>
         <section className='mt-5'>
@@ -35,12 +33,11 @@ const LearningPlatFrom = () => {
         <Link to='/'><button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 dark:focus:ring-gray-400 font-medium rounded-lg text-semibold px-8 py-5 text-center mr-2 mb-2 mt-10">Explore Our Courses</button></Link>
         </section>
       </div>
-      <div className='relative flex ml-24'>
-        <div className='bg-white  h shadow-lg'>
-          <video className='text-2xl text-black absolute' src='https://www.youtube.com/watch?v=i4laq6e_B6U&ab_channel=JhankarMahbub'>Playiodgklsdgjiewajmnklcx</video>
-         
+      <div className='relative lg:flex '>
+        <div  className='lg:mr-24'>
+         <h1 className='card text-black absolute'><span className='inline'>Demo Videos</span>How Can You Starts</h1>
         </div>
-        <img className='w-72 h-72 rounded'  src={img2} alt="mission" />
+        <img className='w-72 h-72 rounded lg:ml-0 ml-8'  src={img2} alt="mission" />
         <img className='w-72 h-72 mt-56 ml-8 rounded-md' src={img1} alt="mission" />
       </div>
     </section>
