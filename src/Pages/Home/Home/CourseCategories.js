34716b3ecf8bab7fbf01b71ab2c000c110a66d2e
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-10-22 22:51:24 +0600

    course data add

--- src/Pages/Home/Home/CourseCategories.js
+++ src/Pages/Home/Home/CourseCategories.js
@@ -1,62 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CourseDetails from './CourseDetails';
 
 const CourseCategories = () => {
-  let courses = [
-    {
-      id: 1,
-      name: 'Machine Learning',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-    {
-      id: 2,
-      name: 'Artificial Intelligence',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-    {
-      id: 3,
-      name: 'Linux Programming',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-    {
-      id: 4,
-      name: 'App Development',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-    {
-      id: 5,
-      name: 'Web Development',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-    {
-      id: 6,
-      name: 'Web Design',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-    {
-      id: 7,
-      name: 'Data Science',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-    {
-      id: 8,
-      name: 'UI/UX Design',
-      course: 64,
-      language: 'Html, Css, JavaScript'
-    },
-  ]
+  const [courses, setCourses] = useState([]);
+ 
+
+  useEffect(() => {
+      fetch('courses.json')
+          .then(res => res.json())
+          .then(data => setCourses(data));
+          
+  }, [])
+  // let courses = [
+  //   {
+  //     id: 1,
+  //     name: 'Machine Learning',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Artificial Intelligence',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Linux Programming',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'App Development',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Web Development',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'Web Design',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'Data Science',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  //   {
+  //     id: 8,
+  //     name: 'UI/UX Design',
+  //     course: 64,
+  //     language: 'Html, Css, JavaScript'
+  //   },
+  // ]
   return (
     <div className='my-20'>
       <h1 className='text-center font-bold text-xl text-green-500'>Start Learning From Today</h1>
       <h1 className='text-center font-bold text-3xl'>Popular Topics To Learn</h1>
-      <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 mt-10 gap-6'>
+      <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-5 mt-10 gap-6'>
         {
           courses.map((course) => <CourseDetails
           key={course.id}
--- src/Pages/Home/Home/CourseDetails.js
+++ src/Pages/Home/Home/CourseDetails.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './CourseDetails.css';
+import { Link } from 'react-router-dom';
 const CourseDetails = ({course}) => {
-  const {name, } = course;
-  console.log(name)
+  const {title, instructor, price, ratings} = course;
+  console.log(title)
   return (
     <div>
-      <div id='bg' className="card w-full bg-purple-300 shadow-xl">
+      <div id='bg' className="card w-full">
   <figure className=" pt-10">
     <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
   </figure>
-  <div className="card-body items-center text-center">
-    <h2 className="card-title">{name}</h2>
-    <p>{}</p>
+  <div className="card-body">
+    <h2 className="card-title text-start font-bold">{title}</h2> 
+    <p className=" text-start">{instructor}</p>
+    <h2 className="text-start">{ratings}</h2>
+    <p className='text-xl font-bold'>$ {price}</p>
     <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
+      <Link><button className="bg-purple-400 px-2 py-1 hover:text-white">Bestsellers</button></Link>
     </div>
   </div>
 </div>
