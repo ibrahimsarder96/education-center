1f5a0f935f81e1a4fc09991c5486f43c4021e268
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-10-24 23:51:41 +0600

    student feedback

--- src/Pages/Home/Home/StudentFeedback.js
+++ src/Pages/Home/Home/StudentFeedback.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
 const StudentFeedback = () => {
+ 
   return (
-    <div>
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-    </div>
+    
+      <section className='my-10'>
+       <h1 className='text-2xl font-bold text-center text-purple-600'>Student Feedback</h1>
+       <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper my-16"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      </section>
   );
 };
 
