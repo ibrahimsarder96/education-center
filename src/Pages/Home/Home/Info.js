b660ad2d793ade7cf4ebdc80628aa72128ab6670
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-10-25 22:56:07 +0600

    student review add

--- src/Pages/Home/Home/Info.js
+++ src/Pages/Home/Home/Info.js
@@ -6,13 +6,13 @@ const Info = () => {
     <div>
       <h1 className='text-3xl text-center font-bold'>Explore Top <span className='text-orange-400'>Categories</span></h1>
       <p></p>
-      <div className='grid grid-cols-1 my-14 gap-4 lg:grid-cols-3'>
-      <CardInfo cardTitle="Machine Learning" bgclass="bg-gradient-to-r from-secondary to-primary" info="4 course"></CardInfo>
-      <CardInfo cardTitle="Artificial Intelligence" bgclass="bg-gradient-to-r from-primary to-secondary" info="2 course"></CardInfo>
-      <CardInfo cardTitle="Linux Programming" bgclass="bg-gradient-to-r from-secondary to-primary" info="1 course"></CardInfo>
-      <CardInfo cardTitle="Web Development" bgclass="bg-gradient-to-r from-secondary to-primary" info="5 course"></CardInfo>
-      <CardInfo cardTitle="Web Design" bgclass="bg-slate-800" info="3 course"></CardInfo>
-      <CardInfo cardTitle="App Development" bgclass="bg-gradient-to-r from-secondary to-primary" info="1 course"></CardInfo>
+      <div className='grid grid-cols-1  gap-6 lg:grid-cols-3'>
+      <CardInfo cardTitle="Machine Learning" bgclass="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600" info="4 course"></CardInfo>
+      <CardInfo cardTitle="Artificial Intelligence" bgclass="bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700" info="2 course"></CardInfo>
+      <CardInfo cardTitle="Linux Programming" bgclass="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600" info="1 course"></CardInfo>
+      <CardInfo cardTitle="Web Development" bgclass="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600" info="5 course"></CardInfo>
+      <CardInfo cardTitle="Web Design" bgclass="bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700" info="3 course"></CardInfo>
+      <CardInfo cardTitle="App Development" bgclass="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600" info="1 course"></CardInfo>
       </div>
     </div>
   );
--- src/Pages/Home/Home/StudentFeedback.js
+++ src/Pages/Home/Home/StudentFeedback.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
@@ -8,25 +8,40 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
 const StudentFeedback = () => {
- 
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    const url = 'reviews.json';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setReviews(data));
+  }, []);
   return (
     
-      <section className='my-10'>
+      <section className='my-24'>
        <h1 className='text-2xl font-bold text-center text-purple-600'>Student Feedback</h1>
        <Swiper
         navigation={true}
         modules={[Navigation]}
         className="mySwiper my-16"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+       
+       {
+        reviews.map(review =>  <SwiperSlide
+        key={review.id}
+        >
+          <div className='mx-20'>
+            <div className='flex items-center justify-start'>
+            <div className="avatar">
+              <div className="w-24 rounded-full">
+                <img className='' src={review.img} alt="" />
+              </div>
+            </div>
+            <h1 className='text-2xl font-bold text-red-400 ml-6'>{review.name}</h1>
+            </div>
+            <p className='mt-5'>{review.details}</p>
+          </div>
+        </SwiperSlide>)
+       }
       </Swiper>
       </section>
   );
