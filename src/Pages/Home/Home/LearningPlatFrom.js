743ee1946635bba121f172f0000ea2d3ea8bdd18
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-11-14 23:50:07 +0600

    video add

--- src/Pages/Home/Home/LearningPlatFrom.js
+++ src/Pages/Home/Home/LearningPlatFrom.js
@@ -5,14 +5,15 @@ import img2 from '../../../assets/achieve/w.jpg';
 import { ImCheckboxChecked } from 'react-icons/im';
 import { MdSlowMotionVideo } from 'react-icons/md';
 import './LearningPlatFrom.css';
+import ReactPlayer from 'react-player';
 
 const LearningPlatFrom = () => {
-  const handleVideo = () =>{
-      console.log('click')
-  }
+  // const handleVideo = () =>{
+  //     console.log('click')
+  // }
   return (
     <div>
-      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-0 gap-4 mb-16 mt-16'>
+      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-16 gap-4 mb-16 mt-16'>
       <div>
         <h5 className='text-green-400 font-bold text-xl'>Best Online Learning Platform</h5>
         <h2 className='text-5xl font-bold my-5'>One Platfrom & Many</h2>
@@ -39,14 +40,14 @@ const LearningPlatFrom = () => {
         </section>
       </div>
       <div className='relative lg:flex'>
-        <div  className='lg:mr-16 '>
-        <MdSlowMotionVideo> <video width="420" height="315"
-src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1">
-</video></MdSlowMotionVideo>
-         <h1 id="card" className='card text-black absolute'><span className='inline'>Demo Videos</span>How Can You Starts</h1>
+        <div  className='sm:'>
+          <ReactPlayer
+          controls
+          width="40vw"
+          height="30vw"
+          url='https://www.youtube.com/watch?v=ifo76VyrBYo&ab_channel=LearningLad'
+          ></ReactPlayer>
         </div>
-        <img className='w-72 h-72 rounded lg:ml-0 ml-8'  src={img2} alt="mission" />
-        <img className='w-72 h-72 mt-56 ml-8 rounded-md' src={img1} alt="mission" />
       </div>
     </section>
     </div>
