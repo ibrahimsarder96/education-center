ccebc72c00378eaf202450c6edcda34ee2388544
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-11-17 23:56:58 +0600

    responsive

--- src/Pages/Home/Home/BannerItem.js
+++ src/Pages/Home/Home/BannerItem.js
@@ -18,7 +18,7 @@ const BannerItem = ({slider}) => {
       <p className='text-white text-xl w-1/2'>Improve your coding skills to the next level by solving tons of fun coding challenges in more than 25 languages and data structures.OnlineExplore lets you practice coding in pure code with online games. You can also learn other programming languages crafted by their top developers. Strengthening your coding skills could also land you a job with their partner companies.</p>
       </div>
       <div className="absolute flex justify-start transform -translate-y-1/2 left-5 top-3/4">
-        <Link to='/'><button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-gray-400 font-medium rounded-lg text-semibold px-8 py-5 text-center mr-2 mb-2">Explore more</button></Link>
+        <Link to='/'><button type="button" className="mobile text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-gray-400 font-medium rounded-lg text-semibold px-8 py-5 text-center mr-2 mb-2 ">Explore more</button></Link>
       </div>
       <div className="flex absolute justify-between transform  left-5 right-5 bottom-64">
         <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
--- src/Pages/Home/Home/Info.js
+++ src/Pages/Home/Home/Info.js
@@ -4,9 +4,8 @@ import CardInfo from './CardInfo';
 const Info = () => {
   return (
     <div>
-      <h1 className='text-3xl text-center font-bold'>Explore Top <span className='text-orange-400'>Categories</span></h1>
-      <p></p>
-      <div className='grid grid-cols-1  gap-6 lg:grid-cols-3'>
+      <h1 className='text-3xl text-center -mb-16 font-bold'>Explore Top <span className='text-orange-400'>Categories</span></h1>
+      <div className='grid grid-cols-1  gap-6 p-0 lg:grid-cols-3'>
       <CardInfo cardTitle="Machine Learning" bgclass="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600" info="4 course"></CardInfo>
       <CardInfo cardTitle="Artificial Intelligence" bgclass="bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700" info="2 course"></CardInfo>
       <CardInfo cardTitle="Linux Programming" bgclass="bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600" info="1 course"></CardInfo>
